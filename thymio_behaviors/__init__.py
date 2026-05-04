e226dd23c6c36952321dcc30c169a8a8b9d9f969
Jérôme Guzzi <jerome@idsia.ch>
2026-05-04 13:51:44 +0200

    Added types

--- thymio_behaviors/__init__.py
+++ thymio_behaviors/__init__.py
@@ -1,20 +1,29 @@
 from __future__ import annotations
 
+import sys
+from collections.abc import Callable
+from typing import Concatenate, ParamSpec, TypeAlias
+
 from .acc import AccBehavior
-from .led_acc import LEDAccBehavior
-from .led_buttons import LEDButtonsBehavior
-from .sound_buttons import SoundButtonsBehavior
 from .explorer import ExplorerBehavior
 from .follower import FollowerBehavior
+from .led_acc import LEDAccBehavior
+from .led_buttons import LEDButtonsBehavior
 from .line import LineFollowingBehavior
+from .protocol import ThymioAsebaProtocol
 from .prox import LEDProxBehavior
 from .prox_comm import LEDProxCommBehavior
+from .sound_buttons import SoundButtonsBehavior
 from .utils import Chain
-from .protocol import ThymioAsebaProtocol
+
+P = ParamSpec('P')
+Callback: TypeAlias = Callable[Concatenate[ThymioAsebaProtocol, P], None]
+Behavior: TypeAlias = Callback[float]
+
 
 __all__ = [
     'LineFollowingBehavior', 'LEDProxBehavior', 'LEDAccBehavior',
     'LEDButtonsBehavior', 'AccBehavior', 'ExplorerBehavior',
     'FollowerBehavior', 'LEDProxCommBehavior', 'SoundButtonsBehavior', 'Chain',
-    'ThymioAsebaProtocol'
+    'ThymioAsebaProtocol', 'Callback', 'Behavior'
 ]
